5d82820f6cdcff3ac2072aa9b5711b08aaa055a9
Farid SAD SAOUD <faridsadsaoud56@gmail.com>
2025-08-03 14:51:07 +0200

    Fix edit button

--- resources/js/Pages/Events/Partials/AddEditDialog.vue
+++ resources/js/Pages/Events/Partials/AddEditDialog.vue
@@ -1,60 +1,70 @@
 <script setup>
-import { ref } from "vue";
+import { ref,watch } from "vue";
 import { useForm } from "@inertiajs/inertia-vue3";
 import Dialog from "@/Components/Common/DialogModal";
 import Button from "@/Components/Common/Button";
 import Input from "@/Components/Common/Input";
 import moment from "moment";
-const emit = defineEmits(["close"]);
+const emit = defineEmits(["close","openAddDialog"]);
 
 const props = defineProps({
+    show: Boolean,
     itemToEdit: {
         type: Object,
         default: null,
     },
 });
 
-const show = ref(false);
 const editing = ref(false);
 
 const form = useForm({
     title: "",
     starts_at: null,
-    ends_at: null,
+    ends_at: null
 });
-
-// Called when the user clicks on the "Add new" button
-const onAddNew = () => {
-    form.reset();
-    show.value = true;
-    editing.value = false;
+const toDateTimeLocal = (date) => {
+    if (!date) return null;
+    return moment(date).format('YYYY-MM-DDTHH:mm');
 };
+const displayDateTime = (date) => {
+    if (!date) return "";
+    return date.replace('T', ' ');
+};
+watch(() => props.itemToEdit, (newVal) => {
+    if (newVal) {
+        form.title = newVal.title;
+       form.starts_at = toDateTimeLocal(newVal.starts_at);
+        form.ends_at = toDateTimeLocal(newVal.ends_at);
+    } else {
+        form.reset();
+    }
+}, { immediate: true });
+
+
+// // Called when the user clicks on the "Add new" button
+// const onAddNew = () => {
+//     form.reset();
+//     editing.value = false;
+// };
 
 // Called when the user submits the form
 const onSubmit = () => {
-     const transformedData = {
+    const payload = {
         title: form.title,
-        starts_at: form.starts_at ? moment(form.starts_at).format("YYYY-MM-DD HH:mm") : null,
-        ends_at: form.ends_at ? moment(form.ends_at).format("YYYY-MM-DD HH:mm") : null,
+        starts_at: form.starts_at ? moment(form.starts_at.replace(' ', 'T')).format('YYYY-MM-DD HH:mm') : null,
+        ends_at: form.ends_at ? moment(form.ends_at.replace(' ', 'T')).format('YYYY-MM-DD HH:mm') : null
     };
-
-    const requestParams = {
-        preserveScroll: true,
-        onSuccess: onClose,
-    };
-
-    // Stores or updates the item
     if (props.itemToEdit) {
-        form.put(route("events.update", props.itemToEdit.id), {
-            ...transformedData,
+        form.put(route('events.update', props.itemToEdit.id), {
+            ...payload,
             preserveScroll: true,
-            onSuccess: () => onClose(),
+            onSuccess: () => emit("close")
         });
     } else {
-        form.post(route("events.store"), {
-            ...transformedData,
+        form.post(route('events.store'), {
+            ...payload,
             preserveScroll: true,
-            onSuccess: () => onClose(),
+            onSuccess: () => emit("close")
         });
     }
 };
@@ -62,21 +72,23 @@ const onSubmit = () => {
 // Called when the dialog closes
 const onClose = () => {
     form.reset();
-    show.value = false;
     emit("close");
 };
+
 </script>
 
 <template>
     <div>
-        <Button @click="onAddNew">
+        <!-- 
+            ouvrir le formulaire d'ajout via un événement émis au parent (`openAddDialog`)
+            La boite de dialogue `Dialog` s'affiche en fonction de la prop `show`. 
+        -->
+        <Button @click="$emit('openAddDialog')">
             <vue-feather type="plus" />
             <span class="ml-2">Add new</span>
         </Button>
-        <Dialog :show="show" @close="onClose">
-            <template #header>{{
-                editing ? "Edit event" : "Add new event"
-            }}</template>
+        <Dialog :show="props.show" @close="$emit('close')">
+            <template #header>{{ itemToEdit ? "Edit event" : "Add new event" }}</template>
 
             <Input
                 name="title"
@@ -88,14 +100,16 @@ const onClose = () => {
                 type="datetime-local"
                 name="starts_at"
                 label="Start Date"
-                v-model="form.starts_at"
+                :value="form.starts_at"
+                @input="form.starts_at = displayDateTime($event.target.value)"
                 class="mb-6"
             />
             <Input
                 type="datetime-local"
                 name="ends_at"
                 label="End Date"
-                v-model="form.ends_at"
+                :value="form.ends_at"
+                @input="form.ends_at = displayDateTime($event.target.value)"
                 class="mb-6"
             />
 
